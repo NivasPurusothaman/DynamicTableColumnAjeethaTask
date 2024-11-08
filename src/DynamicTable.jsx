b549cd20a7e3b7e5780c6fc2d6e27defe613eadb
NivasPurusothaman <nivaspurusothaman24@gmail.com>
2024-11-08 15:46:06 +0530

    image add

--- src/DynamicTable.jsx
+++ src/DynamicTable.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import emptyCircle from "./assets/empty circle.png"
+import partiallyFilledCircle from "./assets/partially filled circle.png"
+import quarterlyFilledCircle from "./assets/quarterly filled circle.png"
 
 function DynamicTable() {
 
@@ -18,9 +21,6 @@ function DynamicTable() {
     },[])
 
     const finalHeaders = firstHeader.concat(headerApiData)
-    // console.log( (Object.keys(bodyApiData[0])).filter( (item) => (item == headerApiData[0].processname) ).toString() )
-    console.log(bodyApiData)
-    console.log(headerApiData)
 
   return (
     <div>
@@ -40,133 +40,247 @@ function DynamicTable() {
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[0].processname) ).toString()
                   == headerApiData[0].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[0].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                    <img src={quarterlyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[1].processname) ).toString()
                   == headerApiData[1].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[1].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[2].processname) ).toString()
                   == headerApiData[2].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[2].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[3].processname) ).toString()
                   == headerApiData[3].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[3].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[4].processname) ).toString()
                   == headerApiData[4].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[4].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[5].processname) ).toString()
                   == headerApiData[5].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[5].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[6].processname) ).toString()
                   == headerApiData[6].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[6].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[7].processname) ).toString()
                   == headerApiData[7].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[7].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[8].processname) ).toString()
                   == headerApiData[8].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[8].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[9].processname) ).toString()
                   == headerApiData[9].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[9].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[10].processname) ).toString()
                   == headerApiData[10].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[10].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[11].processname) ).toString()
                   == headerApiData[11].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[11].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[12].processname) ).toString()
                   == headerApiData[12].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[12].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[13].processname) ).toString()
                   == headerApiData[13].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[13].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[14].processname) ).toString()
                   == headerApiData[14].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[14].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[15].processname) ).toString()
                   == headerApiData[15].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[15].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[16].processname) ).toString()
                   == headerApiData[16].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[16].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[17].processname) ).toString()
                   == headerApiData[17].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[17].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
               <td>
                 {
                   (Object.keys(item)).filter( (item) => (item == headerApiData[18].processname) ).toString()
                   == headerApiData[18].processname ? 
-                  2 : (item.null ? 1 : 0)
+                  (item[headerApiData[18].processname] == "Level 2" ?
+                    <img src={partiallyFilledCircle} /> :
+                    <img src={emptyCircle} />
+                  ) : ((item.null == "Level 1") ? 
+                  <img src={quarterlyFilledCircle} /> :
+                  <img src={emptyCircle} />
+                  )
                 }
               </td>
             </tr>

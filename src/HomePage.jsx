6620f024e8623124f3ee6188684192ce469b845c
NivasPurusothaman <nivaspurusothaman24@gmail.com>
2024-11-12 15:16:02 +0530

    updated nav

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,15 +7,16 @@ import PrimeReactCrud from './PrimeReactCrud';
 function HomePage() {
 
     const [navState, setNavState] = useState(1)
+    const [arrowNum, setArrowNum] = useState(1)
 
   return (
     <div className="homepage">
       <div className="header">
-        <h1>My First Project</h1>
+        <h1>Amphenol</h1>
       </div>
       <div className='navAndContent'>
-        <div className="navbar">
-          <Navbar navState={navState} setNavState={setNavState} />
+        <div className="navbar" style={arrowNum===1 ? {width: "15vw"} : {width : "6.3vw"}}>
+          <Navbar navState={navState} setNavState={setNavState} arrowNum={arrowNum} setArrowNum={setArrowNum} />
         </div>
         <div>
           {(() => {
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
 
-function Navbar({navState, setNavState}) {
+function Navbar({navState, setNavState, arrowNum, setArrowNum}) {
+
   return (
-    <div>
+    <div className='navParentDiv'>
+      <div className='arrow'>
+        {
+          arrowNum === 1 ?
+          <i className='pi pi-chevron-circle-left' style={{fontSize: "30px"}} onClick={()=>setArrowNum(2)}></i> :
+          <i className='pi pi-chevron-circle-right' style={{fontSize: "30px"}} onClick={()=>setArrowNum(1)}></i>
+        }
+      </div>
       <div className="navbarHeader">
-        <h1>Menu</h1>
+        {
+          arrowNum === 1 ?
+          <h1><i className='pi pi-bars btnIcon'></i>Menu</h1> :
+          <h1><i className='pi pi-bars btnIcon'></i></h1>
+        }
       </div>
       <div className='btnDiv'>
         <div className={navState===1 ? "focussedBtn" : null}>
-          <button onClick={()=>setNavState(1)}>First</button>
+          {
+            arrowNum === 1 ?
+            <button onClick={()=>setNavState(1)}><i className='pi pi-users btnIcon'></i>First</button> :
+            <button onClick={()=>setNavState(1)}><i className='pi pi-users btnIcon'></i></button>
+          }
         </div>
       </div>
       <div className='btnDiv'>
         <div className={navState===2 ? "focussedBtn" : null}>
-          <button onClick={()=>setNavState(2)}>Second</button>
+          {
+            arrowNum === 1 ?
+            <button onClick={()=>setNavState(2)}><i className='pi pi-database btnIcon'></i>Second</button> :
+            <button onClick={()=>setNavState(2)}><i className='pi pi-database btnIcon'></i></button>
+          }
         </div>
       </div>
       <div className='btnDiv'>
         <div className={navState===3 ? "focussedBtn" : null}>
-          <button onClick={()=>setNavState(3)}>Third</button>
+          {
+            arrowNum === 1 ?
+            <button onClick={()=>setNavState(3)}><i className='pi pi-gift btnIcon'></i>Third</button> :
+            <button onClick={()=>setNavState(3)}><i className='pi pi-gift btnIcon'></i></button>
+          }
         </div>
       </div>
     </div>
